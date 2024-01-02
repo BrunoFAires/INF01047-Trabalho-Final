0d89a8659fce8e1aba3490a9c12de1fdbb4f6e9e
Bruno Aires <brunoferreiraaires@gmail.com>
2024-01-02 19:04:18 -0300

    first commit

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/bruno/Documentos/Laboratorio_3_Codigo_Fonte/src/glad.c" "CMakeFiles/main.dir/src/glad.c.o" "gcc" "CMakeFiles/main.dir/src/glad.c.o.d"
+  "/home/bruno/Documentos/Laboratorio_3_Codigo_Fonte/src/main.cpp" "CMakeFiles/main.dir/src/main.cpp.o" "gcc" "CMakeFiles/main.dir/src/main.cpp.o.d"
+  "/home/bruno/Documentos/Laboratorio_3_Codigo_Fonte/src/textrendering.cpp" "CMakeFiles/main.dir/src/textrendering.cpp.o" "gcc" "CMakeFiles/main.dir/src/textrendering.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "../bin/Linux/main"
+  "../bin/Linux/main.pdb"
+  "CMakeFiles/main.dir/src/glad.c.o"
+  "CMakeFiles/main.dir/src/glad.c.o.d"
+  "CMakeFiles/main.dir/src/main.cpp.o"
+  "CMakeFiles/main.dir/src/main.cpp.o.d"
+  "CMakeFiles/main.dir/src/textrendering.cpp.o"
+  "CMakeFiles/main.dir/src/textrendering.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
